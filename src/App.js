9258066a1413273bd44543d19dd42d1f5ec085c7
Aids <boris.guba@gmail.com>
2022-04-12 18:33:39 +0300

    my first todo list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,117 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [todoList, setTodoList] = useState([]);
+  const [indexOfEditableItem, setIndexOfIditableItem] = useState(null);
+  const [newInputValue, setNewInputValue] = useState("");
+  /*let indexOfEditableItem = null
+
+  const setIndexOfIditableItem = (value) => {
+    indexOfEditableItem = value
+  }*/
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const newItem = { value: inputValue, checked: false };
+    if (inputValue === "") return;
+    const newTodosArray = todoList.concat([newItem]);
+
+    setTodoList(newTodosArray);
+
+    setInputValue("");
+  };
+
+  const onInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const onRemoveClick = (indexOfRemevingValue) => {
+    const updatedTodosArray = todoList.filter(
+      (todoItem, index) => index !== indexOfRemevingValue
+    );
+    setTodoList(updatedTodosArray);
+  };
+
+  const onEditClick = (indexOfEditValue) => {
+    const editableItem = todoList.find(
+      (todoItem, index) => index === indexOfEditValue
+    );
+    setNewInputValue(editableItem.value);
+    setIndexOfIditableItem(indexOfEditValue);
+  };
+
+  const onNewInputChange = (event) => {
+    setNewInputValue(event.target.value);
+  };
+
+  const onCancelClick = () => {
+    setIndexOfIditableItem(null);
+  };
+
+  const onSaveClick = (saveIndex) => {
+    const newArr = todoList.map((todoItem, index) => {
+      if (index === saveIndex) {
+        return { value: newInputValue, checked: todoItem.checked };
+      }
+
+      return todoItem;
+    });
+
+    setTodoList(newArr);
+    setNewInputValue("");
+    onCancelClick();
+  };
+
+  const onCheckboxClick = (i) => {
+    const newArr = todoList.map((todoItem, index) => {
+      if (index === i) {
+        return { value: todoItem.value, checked: !todoItem.checked };
+      }
+
+      return todoItem;
+    });
+
+    setTodoList(newArr);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={onSubmit}>
+        <input value={inputValue} onChange={onInputChange} />
+        <button>add</button>
+      </form>
+      <div>
+        {todoList.map((todoItem, index) => {
+          return (
+            <div key={index} className="todo-item">
+              {indexOfEditableItem === index ? (
+                <>
+                  <input value={newInputValue} onChange={onNewInputChange} />
+                  <div>
+                    <button onClick={() => onSaveClick(index)}>save</button>
+                    <button onClick={onCancelClick}>cancel</button>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <p>{todoItem.value}</p>
+                  <div>
+                    <input
+                      type="checkbox"
+                      checked={todoItem.checked}
+                      onChange={() => onCheckboxClick(index)}
+                    />
+                    <button onClick={() => onEditClick(index)}>Edit</button>
+                    <button onClick={() => onRemoveClick(index)}>remove</button>
+                  </div>
+                </>
+              )}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
